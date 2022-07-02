2d635d17ce7dbcef6ac363bc69d7fde6180ada9e
yanyuhao <2499787198@qq.com>
2022-07-02 14:57:09 +0800

    查询所有讲师

--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -1,12 +1,15 @@
 package com.atguigu.ggkt.vod.controller;
 
 
+import com.atguigu.ggkt.model.vod.Teacher;
 import com.atguigu.ggkt.vod.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,13 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-07-02
  */
 @RestController
-@RequestMapping("/vod/teacher")
+@RequestMapping("/admin/vod/teacher")
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
 
-
-
+    // http://localhost:8301/admin/vod/teacher/findAll
+    @GetMapping("findAll")
+    public List<Teacher> findAllTeacher(){
+        List<Teacher> list = teacherService.list();
+        return list;
+    }
 }
 
