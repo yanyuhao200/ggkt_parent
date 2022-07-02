2d06604a38f47c311526703db849f0661c56e1a9
yanyuhao <2499787198@qq.com>
2022-07-02 14:52:13 +0800

    搭建项目后台环境

---
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.ggkt.vod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: yyh
+ * @Date: 2022/07/02 14:42
+ */
+
+@SpringBootApplication
+public class ServiceVodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceVodApplication.class,args);
+    }
+}
