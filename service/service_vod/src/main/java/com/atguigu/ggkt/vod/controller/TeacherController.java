e82b2797e891263b3e7fd93086a34e5c5b36b4a5
yanyuhao <2499787198@qq.com>
2022-07-04 14:55:42 +0800

    讲师接口(添加、修改、批量删除

--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -8,6 +8,7 @@ import com.atguigu.ggkt.vod.service.TeacherService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sun.org.apache.regexp.internal.RE;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -60,18 +61,53 @@ public class TeacherController {
             return Result.fail(null);
         }
     }
+    /*//条件查询分页列表
+    @ApiOperation(value = "获取分页列表")
+    @PostMapping("{current}/{limit}")
+    public Result findPage(
+            @ApiParam(name = "current", value = "当前页码", required = true)
+            @PathVariable Long current,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable Long limit,
+            @ApiParam(name = "teacherVo", value = "查询对象", required = false)
+            @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
+        //创建page对象，传递当前页和每页记录数
+        Page<Teacher> pageParam = new Page<>(current, limit);
+        //获取条件值
+        String name = teacherQueryVo.getName();//讲师名称
+        Integer level = teacherQueryVo.getLevel();//讲师级别
+        String joinDateBegin = teacherQueryVo.getJoinDateBegin();//开始时间
+        String joinDateEnd = teacherQueryVo.getJoinDateEnd();//结束时间
+        //封装条件
+        QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(name)) {
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(joinDateBegin)) {
+            wrapper.ge("join_date",joinDateBegin);
+        }
+        if(!StringUtils.isEmpty(joinDateEnd)) {
+            wrapper.le("join_date",joinDateEnd);
+        }
+        //调用方法得到分页查询结果
+        IPage<Teacher> pageModel = teacherService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
+    }*/
 
     // TODO "total": 0,  pages": 0
     // 3、分页条件查询讲师
     @ApiOperation("分页条件查询讲师")
-    @GetMapping("findQueryPage/{current}/{limit}")
+    @PostMapping("findQueryPage/{current}/{limit}")
     public Result findPage(
             @ApiParam(name = "current", value = "当前页码", required = true)
             @PathVariable Long current,
             @ApiParam(name = "limit", value = "每页记录数", required = true)
             @PathVariable Long limit,
             @ApiParam(name = "teacherQueryVo", value = "查询对象", required = false)
-                    TeacherQueryVo teacherQueryVo) {
+            @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
         // 创建page对象
         Page<Teacher> pageParam = new Page<>(current, limit);
         // 判断 TeacherQueryVo 对象是否为空
@@ -103,6 +139,51 @@ public class TeacherController {
             IPage<Teacher> pagModel = teacherService.page(pageParam, wrapper);
             // 返回
             return Result.ok(pagModel);
+        }
+    }
+
+    // 4、添加讲师
+    @ApiOperation("添加讲师")
+    @PostMapping("saveTeacher")
+    public Result saveTeacher(@RequestBody Teacher teacher) {
+        boolean isSuccess = teacherService.save(teacher);
+        if (isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
+    // 5、修改-根据id查询
+    @ApiOperation("根据id查询")
+    @GetMapping("getTeacher/{id}")
+    public Result getTeacher(@PathVariable Long id) {
+        Teacher teacher = teacherService.getById(id);
+        return Result.ok(teacher);
+    }
+
+    // 6、修改-最终实现
+    // json{...} 以对象形式传送
+    @ApiOperation("修改最终实现")
+    @PostMapping("updateTeacher")
+    public Result updateTeacher(@RequestBody Teacher teacher) {
+        boolean isSuccess = teacherService.updateById(teacher);
+        if (isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+    // 7、批量删除讲师
+    // json数组[1,2,3]  以数组形式传递
+    @ApiOperation("批量删除讲师")
+    @PostMapping("removeBatch")
+    public Result removeBatch(@RequestBody List<Long> idList){
+        boolean isSuccess = teacherService.removeByIds(idList);
+        if (isSuccess) {
+            return Result.ok(null);
+        }else {
+            return Result.fail(null);
         }
     }
 }
